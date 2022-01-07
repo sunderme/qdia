69c95698a2eb4e72b182d5c26656a7943d415508
Jan Sundermeyer <sunderme@gmx.de>
2022-01-07 20:57:12 +0100

    fix filenames

--- .github/scripts/package_msys.sh
+++ .github/scripts/package_msys.sh
@@ -5,15 +5,13 @@ set -e
 VERSION_NAME=0.1
 echo "package build into zip for win"
 # workaround for botched qt6 installation
-cp /mingw64/bin/qmake-qt6.exe /mingw64/bin/qmake.exe
-echo "copy dlls and qt5 plugins"
 mkdir -p package-zip
 cp qdia.exe package-zip/
 cd package-zip
-windeployqt-qt6 texstudio.exe
+windeployqt-qt6 qdia.exe
 zip -r ./qdia-win-qt6-${VERSION_NAME}.zip *
 
 cd ..
-sha256sum ./package-zip/texstudio-win-qt6-${VERSION_NAME}.zip
-cp ./package-zip/texstudio-win-qt6-${VERSION_NAME}.zip ./texstudio-${VERSION_NAME}-win-portable-qt6.zip
+sha256sum ./package-zip/qdia-win-qt6-${VERSION_NAME}.zip
+cp ./package-zip/qdia-win-qt6-${VERSION_NAME}.zip ./qdia-${VERSION_NAME}-win-portable-qt6.zip
 
