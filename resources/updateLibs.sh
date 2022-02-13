ff01a6fa0e340e70b32d90523ccecdc907ea3810
Jan Sundermeyer <sunderme@gmx.de>
2022-02-13 20:47:37 +0100

    simplify more

--- resources/updateLibs.sh
+++ resources/updateLibs.sh
@@ -5,6 +5,6 @@ echo "Generate libs.qrc"
 rm ./libs.qrc
 echo "<RCC>">./libs.qrc
 echo "<qresource prefix=\"/\">">>./libs.qrc
-find libs/ -print|grep json|xargs -i echo "<file>"{}"</file>" >> ./libs.qrc
+find libs/ -name *.json -print|xargs -i echo "<file>"{}"</file>" >> ./libs.qrc
 echo "</qresource>">>./libs.qrc
 echo "</RCC>">>./libs.qrc
