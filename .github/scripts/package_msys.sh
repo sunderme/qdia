6b2d0cfea2cf6f8476265114c1ce7a0a97e1d3cc
Jan Sundermeyer <sunderme@gmx.de>
2022-06-14 20:29:37 +0200

    build osx

--- .github/scripts/package_msys.sh
+++ .github/scripts/package_msys.sh
@@ -7,6 +7,7 @@ echo "package build into zip for win"
 # workaround for botched qt6 installation
 mkdir -p package-zip
 cp qdia.exe package-zip/
+cp -r translation package-zip/translations
 cd package-zip
 windeployqt-qt6 qdia.exe
 ldd qdia.exe | awk '{print $3}'| grep ming | xargs -I{} cp -u {} .
