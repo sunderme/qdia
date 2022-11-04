40417119a81017b7eebe20b8f9f1ef42d6990bb2
Jan Sundermeyer <sunderme@gmx.de>
2022-11-04 19:52:46 +0100

    prepare transtion to ubuntu 20.04

--- .github/scripts/package_linux.sh
+++ .github/scripts/package_linux.sh
@@ -9,7 +9,7 @@ wget -c -nv "https://github.com/probonopd/linuxdeployqt/releases/download/contin
 chmod a+x linuxdeployqt-continuous-x86_64.AppImage
 unset QTDIR; unset QT_PLUGIN_PATH ; unset LD_LIBRARY_PATH
 export VERSION=linux-${VERSION_NAME}
-./linuxdeployqt-continuous-x86_64.AppImage appdir/usr/share/applications/*.desktop -bundle-non-qt-libs -appimage
+./linuxdeployqt-continuous-x86_64.AppImage appdir/usr/share/applications/*.desktop -bundle-non-qt-libs -appimage -unsupported-allow-new-glibc
 # ./linuxdeployqt-continuous-x86_64.AppImage appdir/usr/share/applications/*.desktop -appimage
 cp QDia-${VERSION}-x86_64.AppImage ../QDia-${VERSION}-x86_64.AppImage
 
