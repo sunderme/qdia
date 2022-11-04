3c87552928a97964cae0d10095b1a0849dab81f3
Jan Sundermeyer <sunderme@gmx.de>
2022-11-04 20:42:31 +0100

    try linuxdeployqt workaround

--- .github/scripts/package_linux.sh
+++ .github/scripts/package_linux.sh
@@ -9,6 +9,9 @@ wget -c -nv "https://github.com/probonopd/linuxdeployqt/releases/download/contin
 chmod a+x linuxdeployqt-continuous-x86_64.AppImage
 unset QTDIR; unset QT_PLUGIN_PATH ; unset LD_LIBRARY_PATH
 export VERSION=linux-${VERSION_NAME}
+# work-around "secret" obstacle in linuxdeployqt
+mkdir -p appdir/usr/share/doc/libc6
+touch appdir/usr/share/doc/libc6/copyright
 ./linuxdeployqt-continuous-x86_64.AppImage appdir/usr/share/applications/*.desktop -bundle-non-qt-libs -appimage -unsupported-allow-new-glibc
 # ./linuxdeployqt-continuous-x86_64.AppImage appdir/usr/share/applications/*.desktop -appimage
 cp QDia-${VERSION}-x86_64.AppImage ../QDia-${VERSION}-x86_64.AppImage
