3a2d3a4b3ce56e7a4050dfb269ce6dfd80ce075f
ViktorAH14 <ermolovva@gmail.com>
2025-10-04 14:37:06 +0300

    doc(config) add documentation
    Add documentation for config.h files and config.cpp

--- src/config.cpp
+++ src/config.cpp
@@ -1,6 +1,21 @@
+/**
+ * @file config.cpp
+ * @brief Configuration management implementation for QDia application
+ */
+
 #include "config.h"
 #include <QSettings>
 
+/**
+ * @brief Config constructor
+ *
+ * Loads configuration settings from persistent storage using QSettings.
+ * Initializes member variables with stored values or defaults if not present.
+ *
+ * Settings are stored in:
+ * - Organization: QDia
+ * - Application: QDia
+ */
 Config::Config()
 {
     QSettings settings("QDia","QDia");
@@ -8,6 +23,12 @@ Config::Config()
     style=settings.value("view/style", "<default>").toString();
 }
 
+/**
+ * @brief Config destructor
+ *
+ * Saves current configuration settings to persistent storage using QSettings.
+ * Automatically called when the Config object is destroyed.
+ */
 Config::~Config()
 {
     QSettings settings("QDia","QDia");
